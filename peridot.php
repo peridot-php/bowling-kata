993629ce2003383209ef4092eb39af05e3c4d471
Brian Scaturro <scaturrob@gmail.com>
2015-02-19 13:59:13 -0500

    remove unused plugins from peridot.php

--- peridot.php
+++ peridot.php
@@ -1,12 +1,5 @@
 <?php
-
 use Evenement\EventEmitterInterface;
-use Peridot\Plugin\Watcher\WatcherPlugin;
-use Peridot\Reporter\CodeCoverageReporters;
-use Peridot\Reporter\Dot\DotReporterPlugin;
-use Peridot\Reporter\ListReporter\ListReporterPlugin;
-use Peridot\Concurrency\ConcurrencyPlugin;
-use Symfony\Component\Console\Input\InputInterface;
 use Peridot\Console\Environment;
 
 return function(EventEmitterInterface $emitter) {
